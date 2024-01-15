915cd705c2a5fa48773cba967b9ce90a26808763
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2024-01-15 20:43:32 +0530

    customer repo

---
+++ center_1/src/main/java/lk/caffeine/center_1/api/CustomerRepo.java
@@ -0,0 +1,43 @@
+package lk.caffeine.center_1.api;
+
+import lk.caffeine.center_1.util.payload.StandardMessageResponse;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Author: shan
+ * Created: 1/15/24 8:39 PM
+ */
+@RestController("api/v1/customer")
+public class CustomerRepo {
+    @GetMapping
+    @RequestMapping("/getAll")
+    public StandardMessageResponse getAll() {
+        return new StandardMessageResponse(200, "Success", null);
+    }
+
+    @PostMapping
+    @RequestMapping("/save")
+    public StandardMessageResponse save() {
+        return new StandardMessageResponse(200, "Success", null);
+    }
+
+    @PatchMapping(consumes = "application/json")
+    @RequestMapping("/update")
+    public StandardMessageResponse update() {
+        return new StandardMessageResponse(200, "Success", null);
+    }
+
+    @DeleteMapping
+    @RequestMapping("/delete")
+    public StandardMessageResponse delete(@RequestParam String id) {
+        return new StandardMessageResponse(200, "Success", null);
+    }
+
+    @GetMapping
+    @RequestMapping("/existsById")
+    public StandardMessageResponse existsById(@RequestParam String id) {
+        return new StandardMessageResponse(200, "Success", null);
+    }
+
+
+}
